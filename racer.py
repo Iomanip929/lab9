41a47dae7ea1eb7c345386f85f88d1a22fa7550f
Iomanip929 <m_otynbay@kbtu.kz>
2025-04-01 20:27:17 +0200

    Add files via upload

---
+++ racer.py
@@ -0,0 +1,124 @@
+import pygame, sys
+from pygame.locals import *
+import random, time
+pygame.init()
+FPS = 60
+FramePerSec = pygame.time.Clock()
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+COUNT_COIN = 0
+
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+background = pygame.image.load("c:\Program Files (x86)\lab_saves\AnimatedStreet.png")
+DISPLAYSURF = pygame.display.set_mode((400, 600))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Game")
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("c:\Program Files (x86)\lab_saves\Enemy (1).png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0, SPEED)
+        if self.rect.bottom > 600:
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load("c:\Program Files (x86)\lab_saves\player (1).png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if self.rect.left > 0 and pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH and pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("c:\Program Files (x86)\lab_saves\coin.png")
+        self.image = pygame.transform.scale(self.image, (30, 30))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), random.randint(50, 300))
+
+    def move(self):
+        self.rect.move_ip(0, SPEED)
+        if self.rect.top > SCREEN_HEIGHT:
+            self.rect.top = random.randint(-100, -30)
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), self.rect.top)
+
+     
+P1 = Player()
+E1 = Enemy()
+C1 = Coin()
+
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+
+coins = pygame.sprite.Group()
+coins.add(C1)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+all_sprites.add(C1)
+
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+            SPEED += 0.5      
+        if event.type == QUIT:
+            running = False
+
+    DISPLAYSURF.blit(background, (0, 0))
+
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURF.blit(scores, (10, 10))
+
+    coin_score = font_small.render("Coins: " + str(COUNT_COIN), True, BLACK)
+    DISPLAYSURF.blit(coin_score, (SCREEN_WIDTH - 100, 10))
+
+    for entity in all_sprites:
+        entity.move()
+        DISPLAYSURF.blit(entity.image, entity.rect)
+
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('c:\Program Files (x86)\lab_saves\crash.wav').play()
+        pygame.time.delay(1000)
+        DISPLAYSURF.fill(RED)
+        DISPLAYSURF.blit(game_over, (30, 250))
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        pygame.time.delay(2000)
+
+    if pygame.sprite.spritecollideany(P1, coins):
+        COUNT_COIN += 1
+        for coin in coins:
+            coin.rect.top = random.randint(-100, -30)
+            coin.rect.center = (random.randint(40, SCREEN_WIDTH - 40), coin.rect.top)
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
